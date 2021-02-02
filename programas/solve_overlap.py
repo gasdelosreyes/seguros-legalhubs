a658c3c06a3f330f396954c35b9668f0d3f92f7e
Lobón Exequiel <lobonexequiel@gmail.com>
2021-02-02 16:02:18 -0300

    solver final, testeado

--- programas/solve_overlap.py
+++ programas/solve_overlap.py
@@ -1,3 +1,4 @@
+# %%
 from cluster import *
 from cleaner import *
 from stats import *
@@ -47,15 +48,33 @@ def maxLen(a, b):
 		return a[0], b[0]
 	for i in range(len(a) - 1):
 		if len(a[i]) < len(a[i + 1]):
-			max_len = a[i + 1]
-			b_max = b[i + 1]
+			try:
+				max_len = a[i + 1]
+				b_max = b[i + 1]
+			except:
+				max_len = a[i + 1]
+				b_max = b[-1]
 		if len(a[i]) == len(a[i + 1]):
 			max_len = a[-1]
 			b_max = b[-1]
 	return max_len, b_max
 
 
-def solveOverlap(df):
+def resolveLists(l):
+	"""
+	recibe listas sub anidadas y devuelve lista de listas
+	[[[1, 2, 3], [2, 3, 4]], [4,5,6]] == [ [1,2,3], [2,3,4], [4,5,6] ]
+	"""
+
+	if type(l[0]) != list:
+		return [l]
+	aux = []
+	for i in range(len(l)):
+		aux += resolveLists(l[i])
+	return aux
+
+
+def solveOverlap(df, cluster_trash):
 	"""
 	df: DF de entrada que corresponde a los casos clasificados
 	final: DF de salida que corresponde a los gramas no solapados y clusterizado
@@ -77,28 +96,37 @@ def solveOverlap(df):
 		pares = {x: [], y: [], idx_descripcion: [], descripcion: [], cluster: [], responsabilidad: []}
 
 		for i in range(len(df)):
-			if df.loc[i, idx_descripcion] == j and df.loc[i, cluster] != 0:
+			if df.loc[i, idx_descripcion] == j and df.loc[i, cluster] != cluster_trash:
 				pares[x].append(df.loc[i, x])
 				pares[y].append(df.loc[i, y])
 				pares[descripcion].append(df.loc[i, descripcion])
 				pares[cluster].append(df.loc[i, cluster])
 				pares[responsabilidad].append(df.loc[i, responsabilidad])
-
 		if len(pares[descripcion]):
+			oldLen = len(pares[descripcion])
+			pares[descripcion] = resolveLists(pares[descripcion])
 			max_desc = maxLen(pares[descripcion], pares[cluster])[0]
 			max_clust = maxLen(pares[descripcion], pares[cluster])[1]
 			for i in range(len(pares[descripcion])):
 				if compareLists(pares[descripcion][i], max_desc):
-					final[x].append(pares[x][i])
-					final[y].append(pares[y][i])
-					final[idx_descripcion].append(j)
-					final[descripcion].append(pares[descripcion][i])
-					final[cluster].append(pares[cluster][i])
-					final[responsabilidad].append(pares[responsabilidad][i])
+					try:
+						final[x].append(pares[x][i])
+						final[y].append(pares[y][i])
+						final[idx_descripcion].append(j)
+						final[descripcion].append(pares[descripcion][i])
+						final[cluster].append(pares[cluster][i])
+						final[responsabilidad].append(pares[responsabilidad][i])
+					except IndexError:
+						final[x].append(pares[x][-1])
+						final[y].append(pares[y][-1])
+						final[idx_descripcion].append(j)
+						final[descripcion].append(pares[descripcion][i])
+						final[cluster].append(pares[cluster][-1])
+						final[responsabilidad].append(pares[responsabilidad][-1])
 
 	return pd.DataFrame(final)
 
 
-df = pd.read_csv('lado_aseg_dataset-final2.csv')
-df_max = solveOverlap(df)
-df_max.to_csv('lado_aseg_dataset-final2_no-overlap.csv')
+df = pd.read_csv('lado_aseg_dataset-final_filtro-nuevo.csv')
+df_max = solveOverlap(df, cluster_trash=1)
+df_max.to_csv('lado_aseg_dataset-final_filtro-nuevo_no-overlap.csv')

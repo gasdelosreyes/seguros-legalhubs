e6e895d3c00e47cef06d528e22622fc7e7f85404
lobonexequiel <lobonexequiel@gmail.com>
2021-02-04 17:50:28 -0300

    solver final, testeado

--- programas/solve_overlap.py
+++ programas/solve_overlap.py
@@ -4,21 +4,21 @@ from cleaner import *
 from stats import *
 
 
-def compareLists(a: list, b: list) -> int:
+def compareLists(a, b):
 	"""
 	retorna 1 si son iguales, sino 0
 	"""
 	if (len(a) and not len(b)) or (len(b) and not len(a)):
-		return 0
+		return False
 	elif not len(a) and not len(b):
-		return 1
+		return True
 	elif a[0] == b[0]:
 		return compareLists(a[1:], b[1:])
 	else:
-		return 0
+		return False
 
 
-def isInside(a: list, b: list) -> int:
+def isInside(a, b):
 	"""
 	retorna 1 si a esta dentro de b
 	retorna 0 si no es igual o no esta dentro de b
@@ -44,7 +44,7 @@ def maxLen(a, b):
 		if type(a[i][0]) == list:
 			return maxLen(a[i], b)
 
-	if len(a) == 1 and type(a[0]) == list:
+	if len(a) == 1 and type(a[0]) == list:  # [[as,asd,asd]]
 		return a[0], b[0]
 	for i in range(len(a) - 1):
 		if len(a[i]) < len(a[i + 1]):
@@ -102,11 +102,10 @@ def solveOverlap(df, cluster_trash):
 				pares[descripcion].append(df.loc[i, descripcion])
 				pares[cluster].append(df.loc[i, cluster])
 				pares[responsabilidad].append(df.loc[i, responsabilidad])
+
 		if len(pares[descripcion]):
-			oldLen = len(pares[descripcion])
 			pares[descripcion] = resolveLists(pares[descripcion])
 			max_desc = maxLen(pares[descripcion], pares[cluster])[0]
-			max_clust = maxLen(pares[descripcion], pares[cluster])[1]
 			for i in range(len(pares[descripcion])):
 				if compareLists(pares[descripcion][i], max_desc):
 					try:
@@ -127,6 +126,58 @@ def solveOverlap(df, cluster_trash):
 	return pd.DataFrame(final)
 
 
-df = pd.read_csv('lado_aseg_dataset-final_filtro-nuevo.csv')
-df_max = solveOverlap(df, cluster_trash=1)
-df_max.to_csv('lado_aseg_dataset-final_filtro-nuevo_no-overlap.csv')
+df = pd.read_csv('lado_asegurado/lado_aseg_dataset-final_filtro-nuevo.csv')
+df_max = solveOverlap(df, cluster_trash=0)
+df_max.to_csv('lado_aseg_no-overlap.csv')
+
+
+file_name = 'recluster_lado_aseg_no-overlap'
+tokenizedCorpus = df_max['descripcion']
+bowCorpus = [TaggedDocument([' '.join(tup) for tup in words], [idx_tag]) for idx_tag, words in
+             enumerate(tokenizedCorpus)]
+
+fixSeed = 774965317
+np.random.seed(fixSeed)
+
+model = Doc2Vec(bowCorpus, vector_size=300, seed=fixSeed, dm=1, epochs=1000)
+modelVectors = model.docvecs.vectors_docs
+
+pca = PCA(n_components=2, svd_solver='full', whiten=True)
+pcaVectors = pca.fit_transform(modelVectors)
+
+
+n = 1
+n_clusters = 0
+aux = []
+while n_clusters < 0.8:
+	n += 1
+	kmeans = KMeans(n_clusters=n, random_state=fixSeed)
+	kmeans.fit(pcaVectors)
+	n_clusters = silhouette_score(pcaVectors, kmeans.labels_, metric='euclidean')
+	aux.append([n_clusters, n])
+	if 16 <= n:
+		break
+
+pca_df = pd.DataFrame(data=pcaVectors, columns=['x', 'y'])
+
+
+kmeans = KMeans(n_clusters=max(aux)[1], random_state=fixSeed)
+kmeans.fit(pcaVectors)
+
+pca_df = pd.DataFrame(data=pcaVectors, columns=['x', 'y'])
+pca_df['idx_descripcion'] = pd.Series([i for i in df_max['idx_descripcion']])
+
+fig = plt.figure(figsize=(15, 15))
+ax = fig.add_subplot(111)
+print('silhouette_score = ', silhouette_score(pcaVectors, kmeans.labels_, metric='euclidean'))
+ax.grid()
+ax.scatter(pca_df.x, pca_df.y, s=15)
+plt.savefig(file_name + '.png')
+pca_df['descripcion'] = pd.Series(tokenizedCorpus)
+pca_df['cluster'] = pd.Series(kmeans.labels_)
+sns.lmplot('x', 'y', data=pca_df, hue='cluster', fit_reg=False)
+plt.savefig(file_name + '_sns.png')
+pca_df['responsabilidad'] = df['responsabilidad']
+pca_df.to_csv(file_name + '.csv', index=False)
+
+# plotCar(pca_df, 'recluster_colision_distribution', clusters_list=range(10), show=1)

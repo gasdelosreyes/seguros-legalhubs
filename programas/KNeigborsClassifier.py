f9690cc8321aba7469a97227b8a1d93cb33f6330
Lobón Exequiel <lobonexequiel@gmail.com>
2021-03-04 19:34:30 -0300

    Ultimate classificator K-vecinitos_mostrar :)

--- programas/KNeigborsClassifier.py
+++ programas/KNeigborsClassifier.py
@@ -12,7 +12,41 @@ np.random.seed(7)
 Preparacion del dataset
 """
 df = pd.read_csv('tmp/Tabla1.csv')
-dummie_df = pd.get_dummies(df[['impac_position', 'quien', 'movimiento']])
+dummie_df = pd.DataFrame()
+# dummie_df = pd.get_dummies(df[['quien']])
+
+mov = []
+
+for row in df['movimiento']:
+    if row == 'si':
+        mov.append(1)
+    else:
+        mov.append(0)
+print(len(mov))
+
+dummie_df['movimiento'] = pd.Series(mov)
+
+delantera,delantera_derecha,delantera_izquierda,trasera,trasera_derecha,trasera_izquierda = [],[],[],[],[],[]
+for row in df['impac_position']:
+    if row == 'delantera':
+        delantera.append(1)
+    if row == 'delantera izquierda':
+        delantera.append(2)
+    if row == 'delantera derecha':
+        delantera.append(3)
+    if row == 'trasera':
+        delantera.append(4)
+    if row == 'trasera izquierda':
+        delantera.append(5)
+    if row == 'trasera derecha':
+        delantera.append(6)
+
+dummie_df['delantera'] = pd.Series(delantera)
+# dummie_df['delantera_izquierda'] = pd.Series(delantera_izquierda)
+# dummie_df['delantera_derecha'] = pd.Series(delantera_derecha)
+# dummie_df['trasera'] = pd.Series(trasera)
+# dummie_df['trasera_izquierda'] = pd.Series(trasera_izquierda)
+# dummie_df['trasera_derecha'] = pd.Series(trasera_derecha)
 aux = []
 for i in df['responsabilidad']:
     if i == 'COMPROMETIDA' or i == 'DISCUTIDA':
@@ -20,6 +54,17 @@ for i in df['responsabilidad']:
     else:
         aux.append(0)
 dummie_df['responsabilidad'] = pd.Series(aux)
+quien = [] 
+for row in df['quien']:
+    if row == 'asegurado':
+        quien.append(1)
+    else:
+        quien.append(0)
+
+dummie_df['quien'] = pd.Series(quien)
+dummie_df = dummie_df.dropna()
+print(dummie_df.head())
+
 dummie_df = pd.concat([dummie_df[dummie_df['responsabilidad'] == 0], dummie_df[dummie_df['responsabilidad'] == 1].sample(1100)])
 location = ['calle', r'garaje', r'roton\w*', 'autopista', 'avenida', 'cruce', 'cruze', r'esquina\w*', r'estacionami\w*', 'carril', 'ruta', r'semaforo\w*', r'intersec.?', 'tunel', 'peaje']
 
